64bb5477196ff03d8506aa55ff62d077f0040287
Cameron Lai <cameronjml2@gmail.com>
2015-01-10 01:36:50 +0800

    Comment out system manifest

--- app/src/main/java/com/cameronlai/phonehaviour/MainActivity.java
+++ app/src/main/java/com/cameronlai/phonehaviour/MainActivity.java
@@ -19,7 +19,6 @@ import com.androidplot.xy.XYSeries;
 import com.androidplot.xy.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.prefs.Preferences;
 
 public class MainActivity extends ActionBarActivity {
 
