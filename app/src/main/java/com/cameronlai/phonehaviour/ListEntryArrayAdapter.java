2c8dee8407be925c9a21865872933484888bbb05
Cameron Lai <cameronjml2@gmail.com>
2015-01-13 23:07:05 +0800

    Include package icon, package name, time in list entry

--- app/src/main/java/com/cameronlai/phonehaviour/ListEntryArrayAdapter.java
+++ app/src/main/java/com/cameronlai/phonehaviour/ListEntryArrayAdapter.java
@@ -2,6 +2,9 @@ package com.cameronlai.phonehaviour;
 
 import android.app.usage.UsageStats;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,34 +20,49 @@ import java.util.List;
 public class ListEntryArrayAdapter extends ArrayAdapter<UsageStats> {
     private final Context context;
     private final List<UsageStats> values;
+    private PackageManager mPackageManager;
 
     public ListEntryArrayAdapter(Context context, List<UsageStats> values) {
         super(context, R.layout.list_entry, values);
         this.context = context;
         this.values = values;
+        this.mPackageManager = this.context.getPackageManager();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Find all UI elements
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.list_entry, parent, false);
-
         TextView textViewPackageName = (TextView) rowView.findViewById(R.id.firstLine);
-        textViewPackageName.setText(values.get(position).getPackageName());
-
         TextView textViewPackageTime = (TextView) rowView.findViewById(R.id.secondLine);
-        textViewPackageTime.setText(Long.toString(values.get(position).getTotalTimeInForeground()));
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.packageIcon);
 
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        // Declare all information handlers
+        UsageStats mUsageStats = values.get(position);
+        String mPackageName = mUsageStats.getPackageName();
+        ApplicationInfo mApplicationInfo;
+        Drawable mPackageIcon;
 
-        // change the icon for Windows and iPhone
-        String s = values.get(position).getPackageName();
-        if (s.startsWith("iPhone")) {
-            //imageView.setImageResource(R.drawable.no);
-        } else {
-            //imageView.setImageResource(R.drawable.ok);
+        // Set application icon
+        try {
+            mPackageIcon = this.mPackageManager.getApplicationIcon(mPackageName);
+            imageView.setImageDrawable(mPackageIcon);
+        }catch (PackageManager.NameNotFoundException e){
+            // Catch block
         }
+
+        // Set application name
+        try {
+            mApplicationInfo = this.mPackageManager.getApplicationInfo(mPackageName, PackageManager.GET_META_DATA);
+            textViewPackageName.setText(this.mPackageManager.getApplicationLabel(mApplicationInfo));
+        }catch (PackageManager.NameNotFoundException e){
+            // Catch block
+        }
+
+        // Set total run time
+        textViewPackageTime.setText(Long.toString(mUsageStats.getTotalTimeInForeground()));
 
         return rowView;
     }
