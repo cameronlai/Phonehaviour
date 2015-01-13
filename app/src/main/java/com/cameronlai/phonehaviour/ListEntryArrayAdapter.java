72d654a20dda14589c1222fdaf3894af19f1e095
Cameron Lai <cameronjml2@gmail.com>
2015-01-13 23:49:45 +0800

    Add progress bar and fine tune listview layout

--- app/src/main/java/com/cameronlai/phonehaviour/ListEntryArrayAdapter.java
+++ app/src/main/java/com/cameronlai/phonehaviour/ListEntryArrayAdapter.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -21,12 +23,17 @@ public class ListEntryArrayAdapter extends ArrayAdapter<UsageStats> {
     private final Context context;
     private final List<UsageStats> values;
     private PackageManager mPackageManager;
+    private long mTotalUsageTime;
 
     public ListEntryArrayAdapter(Context context, List<UsageStats> values) {
         super(context, R.layout.list_entry, values);
         this.context = context;
         this.values = values;
         this.mPackageManager = this.context.getPackageManager();
+        mTotalUsageTime = 0;
+        for(UsageStats s : values){
+            this.mTotalUsageTime += s.getTotalTimeInForeground();
+        }
     }
 
     @Override
@@ -34,21 +41,24 @@ public class ListEntryArrayAdapter extends ArrayAdapter<UsageStats> {
         // Find all UI elements
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.list_entry, parent, false);
-        TextView textViewPackageName = (TextView) rowView.findViewById(R.id.firstLine);
-        TextView textViewPackageTime = (TextView) rowView.findViewById(R.id.secondLine);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.packageIcon);
+        View mRowView = inflater.inflate(R.layout.list_entry, parent, false);
+        TextView mTextViewPackageName = (TextView) mRowView.findViewById(R.id.packageTitle);
+        TextView mTextViewPackageUsage = (TextView) mRowView.findViewById(R.id.packageUsage);
+        ImageView mImageView = (ImageView) mRowView.findViewById(R.id.packageIcon);
+        ProgressBar mProgressBar = (ProgressBar) mRowView.findViewById(R.id.packageProgressBar);
 
         // Declare all information handlers
         UsageStats mUsageStats = values.get(position);
         String mPackageName = mUsageStats.getPackageName();
+        long mPackageUsageTime = mUsageStats.getTotalTimeInForeground();
+        int mPercentageUsage = (int) ( (double)mPackageUsageTime / this.mTotalUsageTime * 100);
         ApplicationInfo mApplicationInfo;
         Drawable mPackageIcon;
 
         // Set application icon
         try {
             mPackageIcon = this.mPackageManager.getApplicationIcon(mPackageName);
-            imageView.setImageDrawable(mPackageIcon);
+            mImageView.setImageDrawable(mPackageIcon);
         }catch (PackageManager.NameNotFoundException e){
             // Catch block
         }
@@ -56,15 +66,18 @@ public class ListEntryArrayAdapter extends ArrayAdapter<UsageStats> {
         // Set application name
         try {
             mApplicationInfo = this.mPackageManager.getApplicationInfo(mPackageName, PackageManager.GET_META_DATA);
-            textViewPackageName.setText(this.mPackageManager.getApplicationLabel(mApplicationInfo));
+            mTextViewPackageName.setText(this.mPackageManager.getApplicationLabel(mApplicationInfo));
         }catch (PackageManager.NameNotFoundException e){
             // Catch block
         }
 
         // Set total run time
-        textViewPackageTime.setText(Long.toString(mUsageStats.getTotalTimeInForeground()));
+        mTextViewPackageUsage.setText(Long.toString(mPackageUsageTime));
 
-        return rowView;
+        // Set progress bar percentage
+        mProgressBar.setProgress(mPercentageUsage);
+
+        return mRowView;
     }
 }
 
