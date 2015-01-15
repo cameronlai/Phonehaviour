fd24e390badae95fcbea0b5d09aca2ab8c8be4e9
Cameron Lai <cameronjml2@gmail.com>
2015-01-16 00:12:04 +0800

    Remove plot for now and add time range spinner

--- app/src/main/java/com/cameronlai/phonehaviour/MainActivity.java
+++ app/src/main/java/com/cameronlai/phonehaviour/MainActivity.java
@@ -5,10 +5,12 @@ import android.app.usage.UsageStatsManager;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -23,7 +25,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
-    private XYPlot plot;
     ListView mListView;
 
     @Override
@@ -40,7 +41,28 @@ public class MainActivity extends ActionBarActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                String mString = (String) parentView.getItemAtPosition(position);
+                int interval = UsageStatsManager.INTERVAL_DAILY;
+                if (mString == getString(R.string.day)) {
+                    interval = UsageStatsManager.INTERVAL_DAILY;
+                } else if (mString == getString(R.string.week)) {
+                    interval = UsageStatsManager.INTERVAL_DAILY;
+                } else if (mString == getString(R.string.month)) {
+                    interval = UsageStatsManager.INTERVAL_MONTHLY;
+                }
+                updateUsageStatistics(interval);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
         // Confirm usage access
         // String tmpSettingStr = Settings.System.getString( getContentResolver(), Settings.ACTION_USAGE_ACCESS_SETTINGS  );
         // If not, prompt the user to set the usage access settings
@@ -49,7 +71,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
-        updateUsageStatistics();
     }
 
     @Override
@@ -81,11 +102,12 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void updateUsageStatistics()
+    public void updateUsageStatistics(int timeSpan)
     {
+        Log.v("myTAG", "UpdatedStatistics");
         // Get usage statistics from Android OS
         final UsageStatsManager mUsageStatManager = (UsageStatsManager) getApplicationContext().getSystemService("usagestats");
-        final List<UsageStats> stats = mUsageStatManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, 0,  System.currentTimeMillis());
+        final List<UsageStats> stats = mUsageStatManager.queryUsageStats(timeSpan, 0,  System.currentTimeMillis());
 
         List<Number> series1Numbers = new ArrayList<Number>();
         for (int i=0; i<stats.size(); i++) {
@@ -95,42 +117,5 @@ public class MainActivity extends ActionBarActivity {
         mListView = (ListView) findViewById(R.id.main_activity_all_package_name);
         ListEntryArrayAdapter adapter = new ListEntryArrayAdapter(this, stats);
         mListView.setAdapter(adapter);
-
-        // More information about Android Plot
-        // e.g. Dynamic data
-        // http://androidplot.com/docs/quickstart/
-
-        // fun little snippet that prevents users from taking screenshots
-        // on ICS+ devices :-)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE);
-
-        // initialize our XYPlot reference:
-        plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
-
-        // Create a couple arrays of y-values to plot:
-
-
-        // Turn the above arrays into XYSeries':
-        XYSeries series1 = new SimpleXYSeries(
-                series1Numbers,          // SimpleXYSeries takes a List so turn our array into a List
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
-
-
-        // Create a formatter to use for drawing a series using LineAndPointRenderer
-        // and configure it from xml:
-        LineAndPointFormatter series1Format = new LineAndPointFormatter();
-        series1Format.setPointLabelFormatter(new PointLabelFormatter());
-        series1Format.configure(getApplicationContext(),
-                R.xml.line_point_formatter_with_plf1);
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-
-        // reduce the number of range labels
-        plot.setTicksPerRangeLabel(3);
-        plot.setTicksPerDomainLabel(1);
-        plot.getGraphWidget().setDomainLabelOrientation(-45);
     }
 }
