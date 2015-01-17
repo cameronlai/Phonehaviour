344bba2e2859d41d7dce48f1999397a0e4f8860e
Cameron Lai <cameronjml2@gmail.com>
2015-01-17 15:54:06 +0800

    Add personal webpage link, with onClick event

--- app/src/main/java/com/cameronlai/phonehaviour/AboutActivity.java
+++ app/src/main/java/com/cameronlai/phonehaviour/AboutActivity.java
@@ -4,15 +4,18 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import org.apache.http.message.BasicNameValuePair;
 
@@ -29,20 +32,37 @@ public class AboutActivity extends ActionBarActivity {
         Context mContext = getApplicationContext();
         ListView mListView = (ListView) findViewById(R.id.about_list);
 
-        List<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
+        final List<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
         BasicNameValuePair item;
 
-        item = new BasicNameValuePair("Version",  mContext.getString(R.string.version));
+        item = new BasicNameValuePair(mContext.getString(R.string.version_key),
+                mContext.getString(R.string.version_value));
         list.add(item);
 
-        item = new BasicNameValuePair("Author",  "Cameron Lai");
+        item = new BasicNameValuePair(mContext.getString(R.string.author_key),
+                mContext.getString(R.string.author_value));
         list.add(item);
 
-        item = new BasicNameValuePair("Website",  "http://www.cameronnlai.com");
+        item = new BasicNameValuePair(mContext.getString(R.string.website_key),
+                mContext.getString(R.string.website_value));
         list.add(item);
 
         TwoLineListArrayAdapter sa = new TwoLineListArrayAdapter(this, list);
 
         mListView.setAdapter(sa);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BasicNameValuePair item = list.get(position);
+                if (item.getValue().indexOf("http") != -1){
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW);
+                    webIntent.setData(Uri.parse(item.getValue()));
+                    startActivity(webIntent);
+                }
+
+            }
+        });
+
     }
 }
